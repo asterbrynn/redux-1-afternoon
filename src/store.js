49999ee6b0afb29c4130ba0ab333404d07759303
Aster Brynn <aster.gibby@gmail.com>
2019-04-22 14:55:15 -0600

    step 4

--- src/store.js
+++ src/store.js
@@ -1,1 +1,23 @@
+import {createStore} from 'redux';
 
+const initialState = {
+	recipeName: "",
+	recipeCategory: ""
+};
+
+export const UPDATE_NAME = "UPDATE_NAME";
+export const UPDATE_CATEGORY = "UPDATE_CATEGORY"
+
+function reducer(state = initialState, action) {
+	const {type, payload} = action;
+	switch(type) {
+		case UPDATE_NAME:
+			return {...state, recipeName: payload};
+		case UPDATE_CATEGORY:
+			return {...state, recipeCategory: payload};
+		default:
+			return state;
+	}
+}
+
+export default createStore(reducer);
